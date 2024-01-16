e35f971cbd46c6e2c4ff7f43a85bcc22f1085b32
Luciano <luciano@email.com>
2024-01-16 11:25:24 +0100

    header provisional

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,5 +1,26 @@
 
 
 <svelte:head>
-    
+    <title>proyecto</title>
 </svelte:head>
+
+<header class="layout-header">
+    <a href="/">Home</a>
+    <a href="/logIn">LogIn</a>
+    <a href="/signUp">SignUp</a>
+    <a href="/mainPage">Personal</a>
+</header>
+
+<style>
+    a {
+		color: #e9c46a;
+		text-decoration: none;
+	}
+	.layout-header {
+		background-color: #0b6285;
+		color: #f4a261;
+		font-size: 30px;
+		text-align: center;
+		padding: 10px;
+	}
+</style>
