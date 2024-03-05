40402315a3be7ada4bbd406ad486f22211107dda
Luciano <luciano@email.com>
2024-03-05 12:04:07 +0100

    Se ha implementado el loading animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,10 +13,10 @@
 <div class="container">
 {#if $navigating}
     <div class="loading">
-
+        <section class="section"> 
             <span class="loader loader-quart"></span>
             loading...
-        
+        </section>
     </div>
 {:else}
 
@@ -40,19 +40,47 @@
         align-items: center;
         justify-content: center;
     }
+    .loader {
+	position: relative;
+	display: inline-block;
+	width: 50px;
+	height: 50px;
+	vertical-align: middle;
+}
     .loader-quart {
-	border-radius: 50%;
+	border-radius: 50px;
 	border: 6px solid rgba(255,255,255,0.4);
     }
 
     .loader-quart::after {
         content: '';
-        border-radius: 50%;
+        position: absolute;
+        top: -6px; 
+        left: -6px;
+        bottom: -6px;
+        right: -6px;
+        border-radius: 50px;
         border: 6px solid transparent;
         border-top-color: #fff;
         -webkit-animation: spin 1s linear infinite;
         animation: spin 1s linear infinite;
     }
+
+    .section {
+	background-color: transparent;
+	height: 300px;
+	line-height: 300px;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
 
 </style>
 
